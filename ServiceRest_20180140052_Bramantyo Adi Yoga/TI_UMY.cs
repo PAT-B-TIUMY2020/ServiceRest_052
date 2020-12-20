93292062790bc50f5981bc123c4805b2ee6d8307
Bramantyoadi12 <56532145+Bramantyoadi12@users.noreply.github.com>
2020-12-20 22:35:35 +0700

    First Commit

---
+++ ServiceRest_20180140052_Bramantyo Adi Yoga/TI_UMY.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace ServiceRest_20180140052_Bramantyo_Adi_Yoga
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class TI_UMY : ITI_UMY
+    {
+        public string CreateMahasiswa(Mahasiswa mhs)
+        {
+            string msg = "GAGAL";
+            SqlConnection sqlcon = new SqlConnection("Data Source=DESKTOP-9DIL2HS;Initial Catalog=\"TI UMY\";Persist Security Info=True;User ID=sa;Password=bramantyo");
+            string query = String.Format("insert into dbo.Mahasiswa values ('{0}', '{1}', '{2}', '{3}')", mhs.nama, mhs.nim, mhs.prodi, mhs.angkatan);
+            //NIM = '{0}'", nim)
+            // string query = "insert into dbo.Mahasiswa values ('"+mhs.nama+"', '"+mhs.nim+"', '"+mhs.prodi+"', '"+mhs.angkatan+"')";
+            SqlCommand sqlcom = new SqlCommand(query, sqlcon); //yang dikirim ke sql
+
+            try
+            {
+                sqlcon.Open(); // membuka connection sql
+
+                Console.WriteLine(query);
+
+                sqlcom.ExecuteNonQuery(); //mengeksekusi untuk memasukkan data
+
+                sqlcon.Close();
+
+                msg = "Sukses";
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(query);
+                msg = "GAGAL";
+            }
+
+            return msg;
+        }
+
+        public List<Mahasiswa> GetAllMahasiswa()
+        {
+            List<Mahasiswa> mahas = new List<Mahasiswa>();
+
+            SqlConnection sqlcon = new SqlConnection("Data Source=DESKTOP-9DIL2HS;Initial Catalog=\"TI UMY\";Persist Security Info=True;User ID=sa;Password=bramantyo");
+            string query = "select Nama, NIM, Prodi, Angkatan from dbo.Mahasiswa";
+            SqlCommand com = new SqlCommand(query, sqlcon); //yang dikirim ke sql
+
+            try
+            {
+                sqlcon.Open();
+                SqlDataReader reader = com.ExecuteReader(); //mendapatkan data yang telah dieksekusi, dari select. hasil query ditaro di reader
+
+                while (reader.Read())
+                {
+                    Mahasiswa mhs = new Mahasiswa();
+                    mhs.nama = reader.GetString(0); // 0 itu array pertama // ini diambil dari iservice
+                    mhs.nim = reader.GetString(1);
+                    mhs.prodi = reader.GetString(2);
+                    mhs.angkatan = reader.GetString(3);
+
+                    mahas.Add(mhs);
+                }
+                sqlcon.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(query);
+            }
+
+            return mahas;
+        }
+
+        public Mahasiswa GetMahasiswaByNIM(string nim)
+        {
+            Mahasiswa mhs = new Mahasiswa();
+
+            SqlConnection sqlcon = new SqlConnection("Data Source=DESKTOP-9DIL2HS;Initial Catalog=\"TI UMY\";Persist Security Info=True;User ID=sa;Password=bramantyo");
+            string query = string.Format("select Nama, NIM, Prodi, Angkatan from dbo.Mahasiswa where NIM = '{0}'", nim);
+            SqlCommand com = new SqlCommand(query, sqlcon);
+
+            try
+            {
+                sqlcon.Open();
+                SqlDataReader reader = com.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    mhs.nama = reader.GetString(0); // 0 itu array pertama // ini diambil dari iservice
+                    mhs.nim = reader.GetString(1);
+                    mhs.prodi = reader.GetString(2);
+                    mhs.angkatan = reader.GetString(3);
+
+                }
+                sqlcon.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(query);
+            }
+
+            return mhs;
+        }
+    }
+}
